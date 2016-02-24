b954b997f24be70aa21e0391dd867f70e679d42c
Kartik Jain <kartikj2012@gmail.com>
2016-02-24 13:31:37 +0530

    Swift JS bi directional communication working

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+class ViewController: UIViewController, WKNavigationDelegate {
     
     
     
@@ -17,43 +17,16 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
     
     @IBOutlet weak var `switch`: UIButton!
     var webViewController: WebViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         webViewController = self.childViewControllers[0] as! WebViewController
-        var userController:WKUserContentController = WKUserContentController()
-        userController.addScriptMessageHandler(
-            self,
-            name: "callbackHandler"
-        )
-        webViewController!.webCfg.userContentController = userController;
-        let htmlString = "<img id='image' src=\"image-loading.gif\" alt=\"Smiley face\" onclick=\"try{webkit.messageHandlers.callbackHandler.postMessage('Hello from JavaScript')}catch(err){console.log(webkit);console.log('The native context does not exist yet')}\">"
-        
-        let path = NSBundle.mainBundle().pathForResource("resource/images", ofType: "")
-        let requestURL = NSURL(fileURLWithPath : path!)
-        webViewController!.webView!.loadHTMLString(htmlString, baseURL: requestURL)
-//        var url = NSURL(string:"https://www.google.com/")
-//        var req = NSURLRequest(URL:url!)
-//        webViewController.webView!.loadRequest(req)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func `switch`(sender: AnyObject) {
         print("clicked")
-//        var userController:WKUserContentController = WKUserContentController()
         let js:String = "document.getElementById('image').setAttribute('src', 'Mic.png')"
-        
-        var userScript:WKUserScript =  WKUserScript(source: js,
-            injectionTime: WKUserScriptInjectionTime.AtDocumentEnd,
-            forMainFrameOnly: false)
-        print(userScript)
-        // Add the user script to the WKUserContentController instance
-//        userController.addScriptMessageHandler(
-//            self,
-//            name: "callbackHandler"
-//        )
-        
-        // Configure the WKWebViewConfiguration instance with the WKUserContentController
-//        webViewController!.webCfg.userContentController = userController;
         webViewController!.webView!.evaluateJavaScript(js) { (result, error) in
             if error != nil {
                 print(result)
@@ -65,13 +38,6 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func userContentController(userContentController: WKUserContentController!, didReceiveScriptMessage message: WKScriptMessage!) {
-        if(message.name == "callbackHandler") {
-            print("JavaScript is sending a message \(message.body)")
-        }
-    }
-
 
 }
 
--- WebViewController.swift
+++ WebViewController.swift
@@ -9,24 +9,45 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKScriptMessageHandler {
     
     var webView: WKWebView?
     var webCfg:WKWebViewConfiguration = WKWebViewConfiguration()
     
     override func loadView() {
         super.loadView()
+        var contentController = WKUserContentController();
+        contentController.addScriptMessageHandler(
+            self,
+            name: "callbackHandler"
+        )
         
-        self.webView = WKWebView()
+        var config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        
+        self.webView = WKWebView(
+            frame: self.view.bounds,
+            configuration: config
+        )
         self.view = self.webView!
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let htmlString = "<img id='image' src=\"image-loading.gif\" alt=\"Smiley face\" onclick=\"try{webkit.messageHandlers.callbackHandler.postMessage('Hello from JavaScript')}catch(err){console.log(webkit);console.log('The native context does not exist yet')}\">"
+        let path = NSBundle.mainBundle().pathForResource("resource/images", ofType: "")
+        let requestURL = NSURL(fileURLWithPath : path!)
+        self.webView!.loadHTMLString(htmlString, baseURL: requestURL)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func userContentController(userContentController: WKUserContentController!, didReceiveScriptMessage message: WKScriptMessage!) {
+        if(message.name == "callbackHandler") {
+            print("JavaScript is sending a message \(message.body)")
+        }
     }
 
 }
